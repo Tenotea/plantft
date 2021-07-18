4087ecc609cd7cedbe72ce787bd8872d4c164f78
Tenotea <drtenotea@gmail.com>
2021-07-18 06:33:06 +0100

    update(hero): made hero responsive

--- src/components/home/HomeHero.jsx
+++ src/components/home/HomeHero.jsx
@@ -6,7 +6,7 @@ import { DIcon } from "../app/DIcon";
 export const HomeHeroSection = () => {
   return (
     <section className="w-full bg-cover bg-no-repeat bg-right-top flex items-center justify-center" style={{backgroundImage: `url(${homeHeroCover})`}}>
-      <div className="max-w-7xl mx-auto px-5 md:sm-8  py-32" >
+      <div className="max-w-lg sm:max-w-7xl mx-auto px-5 md:sm-8  py-32" >
         <h1 className="text-white text-4xl sm:text-6xl font-bold max-w-lg text-center leading-normal sm:leading-relaxed">
           Welcome to Dvision Network
         </h1>
